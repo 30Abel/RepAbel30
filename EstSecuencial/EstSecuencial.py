9b9396e01b672b446fb6cffdd09bbb5d34edc560
30Abel <>
2021-09-28 17:07:03 +0000

    Lapices

---
+++ EstSecuencial/EstSecuencial.py
@@ -0,0 +1,9 @@
+print("HALLAR EL AREA DE UN TRAPECIO")
+#Datos de entrada
+BM=int(input("Ingrese Base Mayor del Trapecio:"))
+bm=int(input("Ingrese Base Mayor del Trapecio:"))
+h=int(input("Ingrese la altura:"))
+#proceso
+area=((BM*bm)*h)/2
+#Datos de Salida
+print("El area del Trapecio:",area,"m2")
