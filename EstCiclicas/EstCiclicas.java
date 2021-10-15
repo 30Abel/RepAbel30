a7d5641e143d3e60afae29448f084b7a3cdf3590
30Abel <>
2021-10-15 02:38:01 +0000

    QuinceEjercicios

--- EstCiclicas/EstCiclicas.java
+++ EstCiclicas/EstCiclicas.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.util.Random;
+import java.io.Console;
 public class EstCiclicas{
   static Scanner sc = new Scanner(System.in);
 
@@ -32,7 +33,6 @@ public class EstCiclicas{
   }
 
 
-
   static void factorial(){
   //definir variables
   int n, resulF=1;
@@ -49,9 +49,85 @@ public class EstCiclicas{
   //datos de salida
   System.out.println("El factorial de "+n+ " es :" +resulF);
   }
+
+
+  static void sumaParesRangoAB(){
+    //definir variables y datos de entrada
+    System.out.println("Introdusca el valor inicial  A");
+    int a=sc.nextInt();
+    System.out.println("Introdusca el valor final B");
+    int b=sc.nextInt();
+    int suma=0;
+    //proceso
+    for (int i=a; i<=b; i++){
+      if (i%2==0){
+        suma=suma+i;
+      }
+    }
+    //datos de salida
+    System.out.println("La suma de numeros pares es :" +suma);
+  }
+
+
+
+static void menuOpciones(){
+int opcion=0;
+String msg="Eliga el algoritmo que desea probar\n 1=ssss\n2=factorial\n3=sumaParesRangoAB";
+System.out.println(msg);
+opcion=sc.nextInt();
+
+while(opcion!=0){
+    switch(opcion){
+        case 1: contarNumMenorCeroMayorCeroWhile(); break;
+        case 2: factorial(); break;
+        case 3: sumaParesRangoAB(); break;
+        default: System.out.println("Opcion no existe!");
+    }
+  System.out.println(msg);
+  opcion=sc.nextInt();    
+}
+}
+
+ static void validarContrasenha(){
+    //definicion de variables
+    String mensaje, password, usuario, msgrespuesta="";
+    int contador=1;
+    //datos de entrada
+    System.out.println("Ingrese su Usuario:");
+    usuario=sc.next();
+    Console console = System.console();
+    //Proceso
+    do{
+      //System.out.println("Ingrese su Password:");
+      //password=sc.next();        
+      char[] passwordArray = console.readPassword("Ingrese su Password:");  
+      if((String.valueOf(passwordArray)).equals("123456d")){
+        msgrespuesta="En hora buena";
+        System.out.println(msgrespuesta);  
+        //contador=4;
+        break;
+      }else if(contador<3){
+        msgrespuesta="lo siento password equivocado!";
+        System.out.println(msgrespuesta);        
+        // contador=contador+1;
+      }else{
+        msgrespuesta="oportunidades Agotadas";    
+        System.out.println(msgrespuesta);     
+      }
+    contador++;
+    }while(contador<=3);
+     //Datos de salida
+    //System.out.println(msgrespuesta);
+  }
+
+
+
+
   
   public static void main(String [] arg){
    //contarNumMenorCeroMayorCeroWhile();
-   factorial();
+   //factorial();
+   //menuOpciones();
+   validarContrasenha();
   }
 }
